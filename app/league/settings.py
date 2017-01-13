2ce3865ba93b7eea03d99473de29a9c804d8edcd
Andrew Hall <hndrewaall@gmail.com>
2017-01-13 18:43:51 -0500

    Add default root pass and pass through for compose

--- app/league/settings.py
+++ app/league/settings.py
@@ -15,7 +15,7 @@ class Config(object):
     DEBUG_TB_INTERCEPT_REDIRECTS = False
     CACHE_TYPE = 'simple'  # Can be "memcached", "redis", etc.
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    LEAGUE_ROOT_PASS = os.environ.get('LEAGUE_ROOT_PASS')
+    LEAGUE_ROOT_PASS = os.environ.get('LEAGUE_ROOT_PASS', 'root')
 
 
 class ProdConfig(Config):
