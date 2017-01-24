5ca3c44c253a86a47cb55d71cca2df29d98c2324
Andrew Hall <hndrewaall@gmail.com>
2017-01-24 17:57:39 -0500

    Update deploy script to use VERSION

--- deploy.sh
+++ deploy.sh
@@ -4,13 +4,14 @@ APP=league_app
 DB=league_db
 WEB=league_webserver
 REPO=055326413375.dkr.ecr.us-east-1.amazonaws.com
+VERSION=$(cat VERSION)
 
-docker tag $APP:latest $REPO/$APP:latest
-docker tag $DB:latest $REPO/$DB:latest
-docker tag $WEB:latest $REPO/$WEB:latest
+docker tag $APP:$VERSION $REPO/$APP:latest
+docker tag $DB:$VERSION $REPO/$DB:latest
+docker tag $WEB:$VERSION $REPO/$WEB:latest
 
 $(aws ecr get-login)
 
-docker push $REPO/$APP:latest
-docker push $REPO/$DB:latest
-docker push $REPO/$WEB:latest
+docker push $REPO/$APP:$VERSION
+docker push $REPO/$DB:$VERSION
+docker push $REPO/$WEB:$VERSION
