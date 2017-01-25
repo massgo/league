833edf276cfc5587871ea26e01c91218508495c6
Andrew Hall <hndrewaall@gmail.com>
2017-01-25 00:40:31 -0500

    Fix travis script again again again again again

--- travis-scripts/deploy.sh
+++ travis-scripts/deploy.sh
@@ -7,11 +7,9 @@ REPO=055326413375.dkr.ecr.us-east-1.amazonaws.com
 VERSION=$(cat VERSION)
 
 if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ] && [ -z "$TRAVIS_TAG" ]; then
-    docker tag $APP:$VERSION $REPO/$APP:latest
-    docker tag $DB:$VERSION $REPO/$DB:latest
-    docker tag $WEB:$VERSION $REPO/$WEB:latest
-
-    $(aws ecr get-login)
+    docker tag $APP:latest $REPO/$APP:$VERSION
+    docker tag $DB:latest $REPO/$DB:$VERSION
+    docker tag $WEB:latest $REPO/$WEB:$VERSION
 
     docker push $REPO/$APP:$VERSION
     docker push $REPO/$DB:$VERSION
