44713f264bdcef01b7a4a8b7c6386c8c1c619a4b
Andrew Hall <andrew@robinpowered.com>
2017-02-06 16:05:34 -0500

    Fix test script

--- travis-scripts/test.sh
+++ travis-scripts/test.sh
@@ -3,11 +3,10 @@
 APP_DIR=app
 APP_FILE=autoapp.py
 
-pushd $APP_DIR
+cd $APP_DIR
 
 export FLASK_APP=$APP_FILE
 export FLASK_DEBUG=1
-flask lint
-flask test
+flask lint && flask test
 
-popd
+exit ?!
